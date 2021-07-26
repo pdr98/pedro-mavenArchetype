66968e27ebab3bef925fcd5bdd942d31a0eb3331
Jovem DSV <jovem.dsv@triersistemas.corp>
2021-07-26 09:53:52 -0300

    maven

---
+++ src/main/java/br/com/pedro/maven_archetype/pedroarchetype/App.java
@@ -0,0 +1,10 @@
+package br.com.pedro.maven_archetype.pedroarchetype;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
